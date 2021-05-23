ea89e22eb1b7bc50699983ee2b41be5d1d8070cd
Hamid Rostami <hamid.r1988@gmail.com>
2021-05-23 16:37:48 +0430

    Add more tests
    New tests are:
    * test_publish_qos0
    * test_publish_qos1
    * test_newmsg

--- test_client.py
+++ test_client.py
@@ -2,6 +2,7 @@ import pytest
 from client import MQTTClient
 import mqtt_codec.packet as pkt
 from io import BytesIO
+import asyncio
 
 
 @pytest.mark.asyncio
@@ -51,3 +52,62 @@ async def test_subscribe():
     c.feed(f.getvalue())
 
     assert sub_ev.is_set() == True
+
+
+@pytest.mark.asyncio
+async def test_publish_qos0():
+    c = MQTTClient()
+    c.connect("id", True, 30)
+    await c.bytes_to_send()
+
+    pub_ev = c.publish("test/topic", b"payload", 0, False)
+    assert pub_ev == None
+    data1 = await c.bytes_to_send()
+
+    pub = pkt.MqttPublish(1, "test/topic", b"payload", False, 0, False)
+    f = BytesIO()
+    pub.encode(f)
+    data2 = f.getvalue()
+
+    assert data1 == data2
+
+
+@pytest.mark.asyncio
+async def test_publish_qos1():
+    c = MQTTClient()
+    c.connect("id", True, 30)
+    await c.bytes_to_send()
+
+    pub_ev = c.publish("test/topic", b"payload", 1, False)
+    assert pub_ev != None
+    assert isinstance(pub_ev, asyncio.Event)
+    assert pub_ev.is_set() == False
+
+    data1 = await c.bytes_to_send()
+    pub = pkt.MqttPublish(1, "test/topic", b"payload", False, 1, False)
+    f = BytesIO()
+    pub.encode(f)
+    data2 = f.getvalue()
+    assert data1 == data2
+
+    puback = pkt.MqttPuback(1)
+    f = BytesIO()
+    puback.encode(f)
+    c.feed(f.getvalue())
+    assert pub_ev.is_set()
+
+
+@pytest.mark.asyncio
+async def test_newmsg():
+    c = MQTTClient()
+    c.connect("id", True, 30)
+    await c.bytes_to_send()
+
+    pub = pkt.MqttPublish(1, "test/topic", b"payload", False, 0, False)
+    f = BytesIO()
+    pub.encode(f)
+    c.feed(f.getvalue())
+
+    msg = await c.get_messages()
+    assert msg.topic == "test/topic"
+    assert msg.payload == b"payload"
